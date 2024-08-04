cbee43acfd1571257ffd9a96ac4f665e39649a28
difaagh <dhifa.aghisni@gmail.com>
2024-08-04 15:05:06 +0700

    init subcontrollerClass

---
+++ UrlSchemePreferences/USPATLController.m
@@ -0,0 +1,6 @@
+#import <AltList/ATLApplicationListSubcontrollerController.h>
+
+@interface USPATLController : ATLApplicationListSubcontrollerController
+- (NSString *)previewStringForApplicationWithIdentifier:
+    (NSString *)applicationID;
+@end
