fc46bb704013fbcac2578b2b96db17d95444c647
difaagh <dhifa.aghisni@gmail.com>
2024-08-31 16:57:22 +0700

    update USPATLController implementation

--- UrlSchemePreferences/USPATLController.h
+++ UrlSchemePreferences/USPATLController.h
@@ -2,5 +2,11 @@
 #import "USPRootListController.h"
 #import <AltList/ATLApplicationListSubcontrollerController.h>
 
+#define USP_APP_KEY_0 @"CFBundleExecutable"
+#define USP_APP_KEY_1 @"urlSchemes"
+#define USP_APP_KEY_2 @"CFBundleURLSchemes"
+#define USP_APP_KEY_DEFAULT @"default"
+#define USP_APP_KEY_CUSTOM @"custom"
+
 @interface USPATLController : USPRootListController
 @end
--- UrlSchemePreferences/USPATLController.m
+++ UrlSchemePreferences/USPATLController.m
@@ -1,7 +1,4 @@
 #import "USPATLController.h"
-#import <Foundation/Foundation.h>
-
-#define USP_APP_BUNDLE_KEY @"appBundle"
 
 @implementation USPATLController
 - (NSArray *)specifiers {
@@ -11,47 +8,137 @@
         [[[NSBundle bundleWithIdentifier:[self specifier].identifier]
             infoDictionary] objectForKey:@"CFBundleURLTypes"];
 
-    if ([urlTypesArray isKindOfClass:[NSArray class]]) {
-      for (NSDictionary *urlType in urlTypesArray) {
-        if ([urlType isKindOfClass:[NSDictionary class]]) {
-          for (NSString *key in urlType) {
-            id value = [urlType objectForKey:key];
-            if ([key isEqualToString:@"CFBundleURLName"]) {
-              PSSpecifier *specifier = [PSSpecifier
-                  preferenceSpecifierNamed:[NSString stringWithFormat:@"%@: %@",
-                                                                      key,
-                                                                      value]
-                                    target:self
-                                       set:nil
-                                       get:nil
-                                    detail:nil
-                                      cell:PSStaticTextCell
-                                      edit:nil];
-              [_specifiers addObject:specifier];
-            } else if ([key isEqualToString:@"CFBundleURLSchemes"] &&
-                       [value isKindOfClass:[NSArray class]]) {
-              NSArray *urlSchemes = (NSArray *)value;
-              NSUInteger index = 0;
-              for (NSString *scheme in urlSchemes) {
-                PSSpecifier *specifier = [PSSpecifier
-                    preferenceSpecifierNamed:
-                        [NSString stringWithFormat:@"%lu: %@",
-                                                   (unsigned long)index, scheme]
-                                      target:self
-                                         set:nil
-                                         get:nil
-                                      detail:nil
-                                        cell:PSStaticTextCell
-                                        edit:nil];
-                [_specifiers addObject:specifier];
-                index++;
-              }
-            }
-          }
-        }
+    for (NSDictionary *urlTypeDict in urlTypesArray) {
+      NSArray *urlschemes = [urlTypeDict objectForKey:@"CFBundleURLSchemes"];
+      for (NSString *urlScheme in urlschemes) {
+        PSSpecifier *urlSchemeText = [PSSpecifier
+            preferenceSpecifierNamed:urlScheme
+                              target:self
+                                 set:@selector(setPreferenceValue:specifier:)
+                                 get:@selector(readPreferenceValue:)
+                              detail:nil
+                                cell:PSEditTextCell
+                                edit:nil];
+        [_specifiers addObject:urlSchemeText];
       }
     }
   }
   return _specifiers;
+}
+- (NSDictionary *)loadSettings {
+  return [NSDictionary dictionaryWithContentsOfFile:USP_VER_PLIST_WITH_PATH];
+}
+
+- (NSDictionary *)appSettingsForIdentifier:(NSString *)identifier {
+  NSDictionary *settings = [self loadSettings];
+  NSString *appExecName =
+      [NSBundle bundleWithIdentifier:identifier].infoDictionary[USP_APP_KEY_0];
+  return settings[appExecName];
+}
+
+- (NSArray *)urlSchemesArrayFromAppSettings:(NSDictionary *)appSettings {
+  return [appSettings objectForKey:USP_APP_KEY_1];
+}
+
+- (NSArray *)urlSchemesFromURLTypeDict:(NSDictionary *)urlTypeDict {
+  return [urlTypeDict objectForKey:USP_APP_KEY_2];
+}
+
+- (NSDictionary *)urlSchemeDictMatchingName:(NSString *)specifierName
+                                  fromArray:(NSArray *)urlSchemesArray {
+  for (NSDictionary *urlTypeDict in urlSchemesArray) {
+    NSArray *urlSchemes = [self urlSchemesFromURLTypeDict:urlTypeDict];
+    for (NSDictionary *urlDict in urlSchemes) {
+      NSString *def = [urlDict objectForKey:USP_APP_KEY_DEFAULT];
+      if ([specifierName isEqualToString:def]) {
+        return urlDict;
+      }
+    }
+  }
+  return nil;
+}
+
+- (id)readPreferenceValue:(PSSpecifier *)specifier {
+  NSDictionary *appSettings =
+      [self appSettingsForIdentifier:[self specifier].identifier];
+  if (appSettings) {
+    NSArray *urlSchemesArray =
+        [self urlSchemesArrayFromAppSettings:appSettings];
+    NSDictionary *urlSchemeDict =
+        [self urlSchemeDictMatchingName:specifier.name
+                              fromArray:urlSchemesArray];
+    NSString *customScheme = [urlSchemeDict objectForKey:USP_APP_KEY_CUSTOM];
+    return customScheme ?: nil;
+  }
+  return nil;
+}
+- (BOOL)isCloned {
+  NSMutableDictionary *settings = [NSMutableDictionary dictionary];
+  [settings
+      addEntriesFromDictionary:
+          [NSDictionary dictionaryWithContentsOfFile:USP_VER_PLIST_WITH_PATH]];
+  for (NSDictionary *key in settings) {
+    if ([settings[key] isKindOfClass:[NSDictionary class]] &&
+        [settings[key][USP_APP_KEY_0]
+            isEqualToString:[self specifier].identifier]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+- (void)setPreferenceValue:(id)value specifier:(PSSpecifier *)specifier {
+  NSMutableDictionary *settings = [[self loadSettings] mutableCopy];
+  if (!settings) {
+    settings = [NSMutableDictionary dictionary];
+  }
+
+  if (![self isCloned]) {
+    NSString *identifier = [self specifier].identifier;
+    NSDictionary *appInfo =
+        [NSBundle bundleWithIdentifier:identifier].infoDictionary;
+    NSArray *urlTypesArray = [appInfo objectForKey:USP_APP_KEY_1];
+    NSMutableArray *bundleUrlSchemesArray = [[NSMutableArray alloc] init];
+
+    for (NSDictionary *urlTypeDict in urlTypesArray) {
+      NSArray *urlSchemes = [self urlSchemesFromURLTypeDict:urlTypeDict];
+      if (!urlSchemes) {
+        continue;
+      }
+
+      NSMutableArray *urlDictArray = [[NSMutableArray alloc] init];
+      for (NSString *url in urlSchemes) {
+        if (url) {
+          NSMutableDictionary *dictUrl = [NSMutableDictionary dictionary];
+          [dictUrl setObject:url forKey:USP_APP_KEY_DEFAULT];
+          [dictUrl setObject:@"" forKey:USP_APP_KEY_CUSTOM];
+          [urlDictArray addObject:dictUrl];
+        }
+      }
+
+      if ([urlDictArray count] > 0) {
+        [bundleUrlSchemesArray addObject:@{USP_APP_KEY_2 : urlDictArray}];
+      }
+    }
+
+    if ([bundleUrlSchemesArray count] > 0) {
+      NSString *appExecName = [NSBundle bundleWithIdentifier:identifier]
+                                  .infoDictionary[USP_APP_KEY_0];
+      settings[appExecName] = [NSMutableDictionary dictionary];
+      [settings[appExecName] setObject:bundleUrlSchemesArray
+                                forKey:USP_APP_KEY_1];
+    }
+  } else {
+    // TODO: Write value to custom
+  }
+
+  [settings writeToFile:USP_VER_PLIST_WITH_PATH atomically:YES];
+
+  CFStringRef notificationName =
+      (__bridge CFStringRef)specifier.properties[@"PostNotification"];
+  if (notificationName) {
+    CFNotificationCenterPostNotification(
+        CFNotificationCenterGetDarwinNotifyCenter(), notificationName, NULL,
+        NULL, YES);
+  }
 }
 @end
